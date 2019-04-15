4b76a34c15064c68991457a9c9bb14240f6f8b69
Eddie Ruiz <ruizeduardo21@gmail.com>
2019-04-15 09:07:07 -0400

    endsWith coding challenge complete

---
+++ javascriptChallenges/endsWith/app.js
@@ -0,0 +1,27 @@
+const Author = {
+  name: "Eddie Ruiz",
+  title: "Software Engineer",
+  feature: "Video Software Implementing String Verification Through parameters"
+};
+
+let testStr = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel';
+
+let testEnder = 'vel';
+
+let shortTester = 'DataAnalyticsvel';
+
+endsWith = (str, ending) => {
+
+  console.log(`This is the String to decode : ${str}`);
+  console.log(`Checking if input ends with : ${ending} ..`);
+
+for(var i = 0; i < str.length ; i++){
+  
+}
+
+  
+
+};
+
+
+endsWith(shortTester, testEnder);
