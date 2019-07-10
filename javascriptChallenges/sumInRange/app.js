54b74394b87313d6e242711cb964d2d3b8a29678
Eddie Ruiz <ruizeduardo21@gmail.com>
2019-07-10 16:14:43 -0400

    refactored and re did sumInRage function

--- javascriptChallenges/sumInRange/app.js
+++ javascriptChallenges/sumInRange/app.js
@@ -1,29 +1,16 @@
-fsumAll = (arr) => {
+sumAll = (arr) => {
   let starter = arr[0];
   let ender = arr[1];
   let count = 0;
 
-  let result = 0;
 if(starter <= ender){
   for(let i = starter; i <= ender; i++){
-    console.log(i);
-
-    count += i;
-
-  
+    count += i;  
   }
 }else {
-
  for(let i = ender; i <= starter; i++){
-
-
-
    count += i;
  }
-
 }
-
-
-
   return count;
 }
