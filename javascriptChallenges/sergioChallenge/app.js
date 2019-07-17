89194e359c5684223a1970a9e7f22769eeebb646
Eddie Ruiz <ruizeduardo21@gmail.com>
2019-07-17 17:21:44 -0400

    sergio challenge while power outage

---
+++ javascriptChallenges/sergioChallenge/app.js
@@ -0,0 +1,14 @@
+let numOne = 4;
+let numTwo = 6;
+
+sergioCodes = (numOne,numTwo) => {
+
+numOne = numOne + numTwo;
+
+numTwo = numTwo - numOne ;
+
+numOne = numOne - numTwo;
+
+}
+
+sergioCodes(numOne,numTwo);
