d257f7c753b09691ba6e314c24b8450d1e05a80c
eddieruiz <eddi@overseasinternational.com>
2020-01-16 23:57:31 -0500

    hello

--- javascriptChallenges/basicStrMath/app.js
+++ javascriptChallenges/basicStrMath/app.js
@@ -1,7 +1,21 @@
-//MAKE AN ALGORITHIM THAT TAKES A STRING AS A PARAMENTER "1PLUS2MINUS4" AND MAKES THE MATHEMATICAL 
-
-strMath = str => {
-
-}
-
-strMath('1plus2minus4');
+function calculate(str) {
+  //your code here...
+  let strSplit = str.split('');
+  console.log(`STR SPLIT INTO AN ARRAY ${strSplit}`);
+  let counter = 0;
+  let equation = " ";
+  
+  for(i=0;i < strSplit.length; i++){
+    console.log(`CURRENT ITERATION ---- ${strSplit[i]}`);
+    let number = Number(strSplit[i])
+    
+  }
+  
+  
+  let response = Number(str[1]);
+  return response;
+  
+  }
+  
+  calculate("1plus2plus3plus4");
+  // , '10'
