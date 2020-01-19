ac571301d5677b71e32455e7adb41132f77b678c
eddieruiz <eddi@overseasinternational.com>
2020-01-18 19:40:07 -0500

    no console logs

--- javascriptChallenges/basicStrMath/app.js
+++ javascriptChallenges/basicStrMath/app.js
@@ -1,12 +1,10 @@
 function calculate(str) {
   //your code here...
   let strSplit = str.split('');
-  console.log(`STR SPLIT INTO AN ARRAY ${strSplit}`);
   let counter = 0;
   let equation = " ";
   
   for(i=0;i < strSplit.length; i++){
-    console.log(`CURRENT ITERATION ---- ${strSplit[i]}`);
     let number = Number(strSplit[i])
     
   }
